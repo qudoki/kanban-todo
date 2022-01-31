c72a491fa4b7523798d90b0a34e3e7b0c062bf73
Qian Dong <qdong327@gmail.com>
2022-01-31 17:16:58 -0500

    basic drag functionality complete

--- js/view/Dropzone.js
+++ js/view/Dropzone.js
@@ -1,32 +1,51 @@
+import KanbanAPI from "../api/KanbanAPI.js";
+
 export default class Dropzone {
-    static createDropzone() {
-        const range = document.createRange();
+	static createDropzone() {
+		const range = document.createRange();
 
-        range.selectNode(document.body);
+		range.selectNode(document.body);
 
-        const dropzone =  range.createContextualFragment(`
+		const dropzone = range.createContextualFragment(`
             <div class="kanban__dropzone"></div>
         `).children[0];
 
-        dropzone.addEventListener("dragover", e => {
-            e.preventDefault();
-            dropzone.classList.add("kanban__dropzone-active");
-        });
+		dropzone.addEventListener("dragover", (e) => {
+			e.preventDefault();
+			dropzone.classList.add("kanban__dropzone-active");
+		});
 
-        dropzone.addEventListener("dragleave", () => {
-            dropzone.classList.remove("kanban__dropzone-active");
-        });
+		dropzone.addEventListener("dragleave", () => {
+			dropzone.classList.remove("kanban__dropzone-active");
+		});
 
-        dropzone.addEventListener("drop", e => {
-            e.preventDefault();
-            dropzone.classList.remove("kanban__dropzone-active");
+		dropzone.addEventListener("drop", (e) => {
+			e.preventDefault();
+			dropzone.classList.remove("kanban__dropzone-active");
 
-            const columnElement = dropzone.closest(".kanban__column");
-            const columnId = Number(columnElement.dataset.id);
+			const columnElement = dropzone.closest(".kanban__column");
+			const columnId = Number(columnElement.dataset.id);
+			const dropzonesInColumn = Array.from(
+				columnElement.querySelectorAll(".kanban__dropzone")
+			);
+			const droppedIndex = dropzonesInColumn.indexOf(dropzone);
+			const itemId = Number(e.dataTransfer.getData("text/plain"));
+			const droppedItemElement = document.querySelector(
+				`[data-id="${itemId}"]`
+			);
+			const insertAfter = dropzone.parentElement.classList.contains("kanban__item") ? dropzone.parentElement : dropzone;
 
-            console.log(columnElement, columnId);
-        })
+            if (droppedItemElement.contains(dropzone)) {
+                return;
+            }
 
-        return dropzone;
-    }
+            insertAfter.after(droppedItemElement);
+			KanbanAPI.updateItem(itemId, {
+				columnId,
+				position: droppedIndex
+			});
+		});
+
+		return dropzone;
+	}
 }
