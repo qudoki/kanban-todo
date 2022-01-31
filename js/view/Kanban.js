4c08102aec60a3d551af90333fc91d346c1233fd
Qian Dong <qdong327@gmail.com>
2022-01-31 16:28:29 -0500

    buttons working

---
+++ js/view/Kanban.js
@@ -0,0 +1,23 @@
+import Column from "./Column.js";
+
+export default class Kanban {
+	// relates to kanban at html gen by js
+	constructor(root) {
+		this.root = root;
+
+        Kanban.columns().forEach(column => {
+            const columnView = new Column(column.id, column.title);
+
+            this.root.appendChild(columnView.elements.root);
+        });
+	}
+
+	static columns() {
+		return [
+			{ id: 1, title: "Backlog" },
+			{ id: 2, title: "Progress" },
+			{ id: 3, title: "Snag" },
+			{ id: 4, title: "Done" }
+		];
+	}
+}
