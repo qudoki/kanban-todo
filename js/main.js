59877f9548967a48b4f1b5dd6e9aeda072486f86
Qian Dong <qdong327@gmail.com>
2022-02-03 13:13:57 -0500

    added categories

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 import KanbanAPI from "./api/KanbanAPI.js";
 import Kanban from "./view/Kanban.js";
 
-// const data = [{"id":1,"items":[{"id":72714,"content":"Do laundry"}]},{"id":2,"items":[{"id":23044,"content":"Code project"}]},{"id":3,"items":[{"id":61584,"content":"Create art"}]}];
-
-// KanbanAPI.deleteItem(450399);
-
 new Kanban(
     document.querySelector(".kanban")
 );
--- js/view/Item.js
+++ js/view/Item.js
@@ -3,59 +3,68 @@ import KanbanAPI from "../api/KanbanAPI.js";
 import Kanban from "./Kanban.js";
 
 export default class Item {
-    constructor(id, content) {
-        const bottomDropzone = Dropzone.createDropzone();
+	constructor(id, content) {
+		const bottomDropzone = Dropzone.createDropzone();
 
-        this.elements = {};
-        this.elements.root = Item.createRoot();
-        this.elements.input = this.elements.root.querySelector(".kanban__item-input");
+		this.elements = {};
+		this.elements.root = Item.createRoot();
+		this.elements.input = this.elements.root.querySelector(
+			".kanban__item-input"
+		);
 
-        this.elements.root.dataset.id = id;
-        this.elements.input.textContent = content;
-        this.content = content;
+		this.elements.root.dataset.id = id;
+		this.elements.input.textContent = content;
+		this.content = content;
 
-        this.elements.root.appendChild(bottomDropzone);
+		this.elements.root.appendChild(bottomDropzone);
 
-        const onBlur = () => {
-            const newContent = this.elements.input.textContent.trim();
+		const onBlur = () => {
+			const newContent = this.elements.input.textContent.trim();
 
-            if (newContent == this.content) {
-                return;
-            }
-            this.content = newContent;
-            KanbanAPI.updateItem(id, {
-                content: this.content
-            });
-        };
-        this.elements.input.addEventListener("blur", onBlur);
-        this.elements.root.addEventListener("dblclick", () => {
-            const check = confirm("Are you sure you want to delete this item?");
-            if (check) {
-                KanbanAPI.deleteItem(id);
-                this.elements.input.removeEventListener("blur", onBlur);
-                this.elements.root.parentElement.removeChild(this.elements.root);
-            }
-        });
+			if (newContent == this.content) {
+				return;
+			}
+			this.content = newContent;
+			KanbanAPI.updateItem(id, {
+				content: this.content,
+			});
+		};
+		this.elements.input.addEventListener("blur", onBlur);
+		this.elements.root.addEventListener("dblclick", () => {
+			const check = confirm("Are you sure you want to delete this item?");
+			if (check) {
+				KanbanAPI.deleteItem(id);
+				this.elements.input.removeEventListener("blur", onBlur);
+				this.elements.root.parentElement.removeChild(this.elements.root);
+			}
+		});
 
-        this.elements.root.addEventListener("dragstart", e => {
-            e.dataTransfer.setData("text/plain", id);
-        });
-        this.elements.input.addEventListener("drop", e => {
-            // Prevents default - dragging text into other item
-            e.preventDefault();
-        });
-    }
-    static createRoot() {
-        const range = document.createRange();
+		this.elements.root.addEventListener("dragstart", (e) => {
+			e.dataTransfer.setData("text/plain", id);
+		});
+		this.elements.input.addEventListener("drop", (e) => {
+			// Prevents default - dragging text into other item
+			e.preventDefault();
+		});
+	}
+	static createRoot() {
+		const range = document.createRange();
 
 		range.selectNode(document.body);
 
 		return range.createContextualFragment(`
             <div class="kanban__item" draggable="true">
             <div class="kanban__item-input" contenteditable></div>
+                <div class="category__div">
+                    <button id="cat__mebic" class="item__category" type="button">Mebic</button>
+                    <button id="cat__home" class="item__category type="button"">Home</button>
+                    <button id="cat__delhi" class="item__category type="button"">Delhi</button>
+                    <button id="cat__work" class="item__category type="button"">Work</button>
+                    <button id="cat__misc" class="item__category type="button"">Misc</button>
+                </div>
             </div>
             `).children[0];
-        }
-    }
-    // TODO: sorting by priority and categorize with color, how to deal with timescale?
-    // <div class="priority">Priority: 1</div>
+	}
+}
+// TODO: sorting by priority and categorize with color, how to deal with timescale?
+// <div class="priority">Priority: 1</div>
